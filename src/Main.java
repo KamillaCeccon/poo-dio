0ed2e5d0e4c9d3449ce7c231b80e7f7160ed8c1a
KamiCeccon <kamiceccon@icloud.com>
2024-07-24 22:43:49 -0300

    implementando regras de negócio

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
 
 import java.time.LocalDate;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Main {
     public static void main(String[] args) {
@@ -22,10 +22,38 @@ public class Main {
         mentoria.setDescricao("descrição Mentoria java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria);
-       
+//        System.out.println(curso1);
+//        System.out.println(curso2);
+//        System.out.println(mentoria);
+
+        BootCamp bootCamp = new BootCamp();
+        bootCamp.setNome("Bootcamp Java Developer");
+        bootCamp.setDescricao("Descrição BootCamp Java Dev");
+        bootCamp.getConteudos().add(curso1);
+        bootCamp.getConteudos().add(curso2);
+        bootCamp.getConteudos().add(mentoria);
+
+        Dev devKamilla = new Dev();
+        devKamilla.setNome("Kamilla");
+        devKamilla.inscreverBootcamp(bootCamp);
+        System.out.println("Conteudos Inscritos Kamilla " + devKamilla.getConteudoInscrito());
+        devKamilla.progredir();
+        System.out.println("__");
+        System.out.println("Conteudos Conclúidos Kamilla " + devKamilla.getConteudoConcluido());
+        System.out.println("XP " + devKamilla.calcularTotalXp());
+        System.out.println("_____");
+
+
+        Dev devJoao = new Dev();
+        devJoao.setNome("João");
+        devJoao.inscreverBootcamp(bootCamp);
+
+        System.out.println("Conteudos Inscritos João " + devJoao.getConteudoInscrito());
+        devJoao.progredir();
+        System.out.println("__");
+        System.out.println("XP " + devJoao.calcularTotalXp());
+        System.out.println("Conteudos Conclúidos João " + devJoao.getConteudoConcluido());
+
 
     }
 }
